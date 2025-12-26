918ee6ebed3d08ab13653df54a5e8e091def6444
Steve Scholnick <scholnicks@gmail.com>
2025-12-26 10:04:51 -0500

    Added more unit tests

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-from chname.__main__ import append, arguments, prepend
+from chname.__main__ import append, arguments, prepend, lower, substitute, remove
 
 
 def test_append(tmp_path):
@@ -14,8 +14,6 @@ def test_append(tmp_path):
     arguments["--verbose"] = False
 
     append()
-
-    assert not f.exists()
     assert (tmp_path / f"{f.name}_suf").exists()
 
 
@@ -29,6 +27,47 @@ def test_prepend(tmp_path):
     arguments["--verbose"] = False
 
     prepend()
-
-    assert not f.exists()
     assert (tmp_path / f"pre_{f.name}").exists()
+
+
+def test_lower(tmp_path):
+    f = tmp_path / "Another.txt"
+    f.write_text("data")
+    arguments.clear()
+    arguments["<files>"] = [str(f)]
+    arguments["--dry-run"] = False
+    arguments["--verbose"] = False
+
+    lower()
+    assert (tmp_path / f"{f.name.lower()}").exists()
+
+
+def test_substitute(tmp_path):
+    f = tmp_path / "an111other.txt"
+    f.write_text("data")
+    arguments.clear()
+    arguments["<files>"] = [str(f)]
+    arguments["<old>"] = "111"
+    arguments["<new>"] = "222"
+    arguments["--dry-run"] = False
+    arguments["--verbose"] = False
+
+    substitute()
+    assert (tmp_path / "an222other.txt").exists()
+
+
+def test_remove(tmp_path):
+    f = tmp_path / "an111other.txt"
+    f.write_text("data")
+    arguments.clear()
+    arguments["<files>"] = [str(f)]
+    arguments["<pattern>"] = "111"
+    arguments["--dry-run"] = False
+    arguments["--verbose"] = False
+
+    remove()
+    assert (tmp_path / "another.txt").exists()
+
+
+
+
